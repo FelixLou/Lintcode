a3c6720685aaef1a9a6f533f733c7acc6e48d8b9
FelixLou <jiabinlu325204@gmail.com>
2015-06-06 12:54:58 -0400

    Update Add Binary.java

--- medium/Add Binary.java
+++ medium/Add Binary.java
@@ -1,4 +1,5 @@
 Question: http://www.lintcode.com/en/problem/add-binary/
+//char to int: char - '0' or Character.getNumericValue(c)
 public class Solution {
     /**
      * @param a a number
@@ -52,3 +53,34 @@ public String addBinary(String a, String b) {
     }
     return (tmp == 0)? result: "1"+result;
 }
+/**********************************************************/
+   public String addBinary(String a, String b) {
+        if (a==null ||a.length()==0){
+            return b;
+        }
+        
+        if (b==null || b.length()==0){
+            return a;
+        }
+        
+       StringBuilder sb=new StringBuilder();
+       
+        
+        int lastA=a.length()-1;
+        int lastB=b.length()-1;
+        int carry=0;
+        
+        
+        while (lastA>=0 ||lastB>=0 ||carry>0){
+            int num1=lastA>=0?a.charAt(lastA--)-'0':0;
+            int num2=lastB>=0?b.charAt(lastB--)-'0':0;
+            int current=(num1+num2+carry)%2;
+            carry=(num1+num2+carry)/2;
+            
+            sb.insert(0, current);
+            
+            
+        }
+        
+        return sb.toString();
+    }
